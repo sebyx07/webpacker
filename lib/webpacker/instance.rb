bbb4b4ad9143a250c148862d92be0b9a2d2a6aa0
sebi <sebyx07.pro@gmail.com>
2020-03-16 17:24:22 +0000

    change

--- lib/webpacker/instance.rb
+++ lib/webpacker/instance.rb
@@ -1,5 +1,6 @@
 class Webpacker::Instance
-  cattr_accessor(:logger) { ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT)) }
+  # cattr_accessor(:logger) { ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT)) }
+  cattr_accessor(:logger) { ActiveSupport::TaggedLogging.new(Logger.new(STDOUT)) }
 
   attr_reader :root_path, :config_path
 
